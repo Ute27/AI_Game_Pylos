2c25505227321fafac5c85edfd14e935b93846e0
Ute27 <75572935+Ute27@users.noreply.github.com>
2022-02-25 09:48:24 +0100

    doMove van RandomPlayer

--- pylos-student/src/main/java/be/kuleuven/pylos/player/student/StudentPlayerRandomFit.java
+++ pylos-student/src/main/java/be/kuleuven/pylos/player/student/StudentPlayerRandomFit.java
@@ -7,28 +7,41 @@ import be.kuleuven.pylos.game.PylosSphere;
 import be.kuleuven.pylos.player.PylosPlayer;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 /**
  * Created by Ine on 5/05/2015.
  */
-public class StudentPlayerRandomFit extends PylosPlayer{
+public class StudentPlayerRandomFit extends PylosPlayer {
+
+    public PylosLocation getRandomFeasibleLocation(PylosGameIF game, PylosBoard board) {
+        List<PylosLocation> options = new ArrayList<>();
+        for (PylosLocation pl : board.getLocations()) {
+            if (board.getReserve(this).canMoveTo(pl)) {
+                options.add(pl);
+            }
+        }
+        int random = (int) Math.random() * options.size();
+        return options.get(random);
+    }
 
     @Override
     public void doMove(PylosGameIF game, PylosBoard board) {
-		/* add a reserve sphere to a feasible random location */
-
+        /* add a reserve sphere to a feasible random location */
+        PylosLocation pl = getRandomFeasibleLocation(game, board);
+        game.moveSphere(board.getReserve(this), pl);
     }
 
     @Override
     public void doRemove(PylosGameIF game, PylosBoard board) {
-		/* removeSphere a random sphere */
+        /* removeSphere a random sphere */
 
     }
 
     @Override
     public void doRemoveOrPass(PylosGameIF game, PylosBoard board) {
-		/* always pass */
+        /* always pass */
 
     }
 }
